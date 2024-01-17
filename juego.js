15f81426b889619c6717d6344d523a91e36861da
JuanjoCanoGil <jcg0039@alu.medac.es>
2024-01-17 10:58:06 +0100

    Logica terminada y js del juego aislado

--- juego.js
+++ juego.js
@@ -1,37 +1,110 @@
 //Se crean las cartas que se pasan por parametro
 function crearTablero(){
-    let url = "";
-    for(let i = 0; i<location.href.split("/").length -1; i++){
-        url += location.href.split("/")[i]+"/";
-    }
-    url += "/index.html";
-    //Extraemos las cartas del localStorage
+    document.write("<h1 class='puntos'>Puntos: 0</h1>");
     
     let nCartas = localStorage.getItem("cartas");
-    document.write("<div class='wrapCartas'>");
+    document.write(`<div class='wrapCartas''>`);
     for(let i = 0; i<nCartas; i++){
-        document.write("<div class='carta'>"+nCartas+"</div>");
+        document.write(`<div class='carta' id='${i}'></div>`);
     }
     document.write("</div>");
 }
 
+(function(){    
+    //Obtenemos las cartas y el tiempo del localStorage
+    let nCartas = localStorage.getItem("cartas");
+    let contador = localStorage.getItem("tiempo");
+    crearTablero()
 
-
-crearTablero()
-
-for(let carta of document.getElementsByClassName("carta")){
-    console.log(carta.textContent);
-}
-
-
-//Cuanta atras hasta terminar la partida
-let contador = localStorage.getItem("tiempo");
-setInterval(
-    function(){
-        document.querySelector("#tiempo").innerHTML = "Tiempo: "+contador;
-        if(contador > 0){
-            contador--;
+    //Asignamos los numeros a las cartas
+    let cont = 0;
+    let controlBucle = 0;
+    let cartas = document.getElementsByClassName("carta");
+    while(cont <= (nCartas/2)){
+        let rand1 = Math.floor(Math.random()*nCartas);
+        let rand2 = Math.floor(Math.random()*nCartas);
+        if(cartas[rand1].value !== undefined || cartas[rand2].value !== undefined || rand1 === rand2){
+            rand1 = Math.floor(Math.random()*nCartas);
+            rand2 = Math.floor(Math.random()*nCartas);
+        }else{
+            cartas[rand1].value = cont;
+            cartas[rand2].value = cont;
+            cont++;
         }
-    },
-    1000
-)
+        controlBucle++;
+        if(controlBucle >= 1000){
+            break;
+        }
+    }
+
+    let uno = null, dos = null,puntos = 0;
+    for(let i = 0; i<cartas.length; i++){
+        cartas[i].addEventListener("click",function(){
+            if(puntos < cartas.length/2 && contador > 0){
+                if(uno == null && cartas[i].value != -1){
+                    uno = cartas[i];
+                    cartas[i].style.backgroundColor = "white";
+                    cartas[i].innerHTML = cartas[i].value;
+                }else{
+                    if(dos == null && cartas[i].value != -1){
+                        dos = cartas[i];
+                        cartas[i].style.backgroundColor = "white";
+                        cartas[i].innerHTML = cartas[i].value;
+                    }else{
+                        if(uno.value == dos.value && dos.id != uno.id){
+                            //Sumamos puntos pero no reiniciamos los estilos
+                            puntos++;
+                            document.getElementsByClassName("puntos")[0].innerHTML = "Puntos: "+puntos
+                            uno = null;
+                            dos = null;
+                            dos.value = uno.value = -1
+                        }else{
+                            //Reiniciamos los estilos en caso de no ser
+                            dos.style.backgroundColor = uno.style.backgroundColor = "black";
+                            dos.innerHTML = uno.innerHTML = "";
+                            uno = null;
+                            dos = null;
+                        }
+                    }
+                }
+            }
+        });
+        document.body.addEventListener("click",function(){
+            if(puntos < cartas.length/2 && contador > 0){
+                if(uno != null && dos != null){
+                    if(uno.value == dos.value && dos.id != uno.id){
+                        //Sumamos puntos pero no reiniciamos los estilos
+                        puntos++;
+                        document.getElementsByClassName("puntos")[0].innerHTML = "Puntos: "+puntos
+                        uno = null;
+                        dos = null;
+                    }else{
+                        //Reiniciamos los estilos en caso de no ser
+                        setTimeout(function(){
+                            dos.style.backgroundColor = uno.style.backgroundColor = "black";
+                            dos.innerHTML = uno.innerHTML = "";
+                            uno = null;
+                            dos = null;
+                        },500)
+                    }
+                }
+            }
+        })
+    }
+
+    //Cuanta atras hasta terminar la partida
+    if(contador == null || isNaN(contador) || !isFinite(contador)){
+        contador = 0;
+    }
+    setInterval(
+        function(){
+            if(puntos < (localStorage.getItem("cartas")/2)){
+                document.querySelector("#tiempo").innerHTML = "Tiempo: "+contador;
+                if(contador > 0 ){
+                    contador--;
+                }
+            }
+        },
+        1000
+    )
+})()
